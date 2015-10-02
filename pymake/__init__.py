e5e1ea01f44fdc995567006e2fa040541c20822b
dbarsam <dbarsam@gmail.com>
2015-10-01 23:01:45 -0400

    Added PymakeSuite and PymakeConfigParser to package __init__.py file.

--- pymake/__init__.py
+++ pymake/__init__.py
@@ -4,5 +4,7 @@ from pymake.solution import PymakeSolution
 from pymake.interpreter import PymakeInterpreter
 from pymake.register import PyRegisterable, PyRegisterCommand
 from pymake.writer import PymakeWriter, PyWritable, PyWriteCommand
+from pymake.suite import PymakeSuite
 from pymake.util.logger import PymakeLogger
 from pymake.util.timer import PymakeTimer
+from pymake.util.config import PymakeConfigParser
