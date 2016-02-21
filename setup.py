24dcd51d469abc8ef80e65a9f9845895ba27014c
dbarsam <dbarsam@gmail.com>
2016-02-21 00:10:24 -0500

    Added initial setup.py and setup.cfg to vsgen package.

---
+++ setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+vsgen's setup.py
+
+For more details see https://packaging.python.org/en/latest/distributing/#setup-args
+"""
+from os import path
+from sys import version_info
+from setuptools import setup, find_packages
+from codecs import open
+
+ROOT_PATH = path.abspath(path.dirname(__file__))
+
+if version_info < (3,):
+    INSTALL_REQUIREMENTS = ['configparser']
+else:
+    INSTALL_REQUIREMENTS = []
+
+TEST_REQUIREMENTS = [
+    'pep8'
+]
+
+SETUP_REQUIREMENTS = [
+    'setuptools-pep8',
+    'setuptools-lint'
+]
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development ',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Office/Business :: Groupware',
+]
+
+ENTRY_POINTS = {
+    'console_scripts': [
+        'vsgen = vsgen.__main__:main'
+    ]
+}
+
+PACKAGES = find_packages(exclude=['contrib', 'docs', 'tests', '.eggs'])
+
+LONG_DESCRIPTION = open(path.join(ROOT_PATH, 'README.rst'), encoding='utf-8').read()
+
+setup(
+    name='vsgen',
+    version='1.0.0',
+    description='A Microsoft Visual Studio solution and project generator pyackage.',
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/dbarsam/python-vsgen',
+    author='dbarsam',
+    author_email='dbarsam@gmail.com',
+    license='MIT',
+    setup_requires=SETUP_REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    keywords='visual studio project generation',
+    packages=PACKAGES,
+    test_suite='tests',
+    tests_require=TEST_REQUIREMENTS,
+    entry_points=ENTRY_POINTS,
+    install_requires=INSTALL_REQUIREMENTS
+)
