154bf1757172e4bf1a6adc4012ca98571fe297bb
dbarsam <dbarsam@gmail.com>
2016-02-28 20:37:28 -0500

    Added __version__ value to vsgen's main __init__.py file.

--- vsgen/__init__.py
+++ vsgen/__init__.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+Main __init__.py for the vsgen package
+"""
+import pkg_resources
+try:
+    pkg = pkg_resources.get_distribution("vsgen")
+    __version__ = pkg.version
+except pkg_resources.DistributionNotFound:
+    __version__ = "0.0.0.0"
+
 from vsgen.solution import VSGSolution
 from vsgen.register import VSGRegisterable, VSGRegisterCommand
 from vsgen.writer import VSGWriter, VSGWritable, VSGWriteCommand
@@ -8,3 +18,18 @@ from vsgen.ptvs.project import PTVSProject
 from vsgen.util.logger import VSGLogger
 from vsgen.util.timer import VSGTimer
 from vsgen.util.config import VSGConfigParser
+
+__all__ = [
+    'VSGSolution',
+    'VSGRegisterable',
+    'VSGRegisterCommand',
+    'VSGWriter',
+    'VSGWritable',
+    'VSGWriteCommand',
+    'VSGSuite',
+    'PTVSInterpreter',
+    'PTVSProject',
+    'VSGLogger',
+    'VSGTimer',
+    'VSGConfigParser'
+]
